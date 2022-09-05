ce164bf6cbd6c358805264d1e12f6e8faabbd71a
Helgi <ojiegoff@gmail.com>
2022-09-05 22:15:25 +0500

    рандом в третьей задаче

--- exampl003/Program.cs
+++ exampl003/Program.cs
@@ -1,4 +1,6 @@
-﻿int num_A = 3;
-int num_B = 5;
+﻿int num_A = new Random().Next(1,10);
+Console.WriteLine(num_A);
+int num_B = new Random().Next(1,10);
+Console.WriteLine(num_B);
 int sum = num_A + num_B;
 Console.WriteLine(sum);
