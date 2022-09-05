041a33d4b62bd7f27cff0c7251847812c40ce173
Helgi <ojiegoff@gmail.com>
2022-09-05 22:39:21 +0500

    приветствие Аве Мария!

---
+++ exampl005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя, путник");
+string user = Console.ReadLine();
+
+if (user.ToLower() == "маша")
+{
+    Console.WriteLine("Аве Мария!");
+}
+else 
+{
+    Console.Write("Привет тебе путник - ");
+    Console.WriteLine(user);
+}
